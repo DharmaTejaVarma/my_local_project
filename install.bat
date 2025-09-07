60e949f7d66be496716cb797f8b2fb8fdfa2b158
Dharma Teja Varma <dharmatejavarma848@gmail.com>
2025-09-07 12:18:57 +0530

    Remove all files github repo only

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing Study Planner Dependencies...
-echo.
-call npm install
-echo.
-echo Installation complete!
-echo Run start-dev.bat to start the development server.
-pause
--- start-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Study Planner Development Server...
-echo.
-echo Installing dependencies...
-call npm install
-echo.
-echo Starting development server...
-call npm run dev
-pause
